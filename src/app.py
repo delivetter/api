4154d9b50ed4eb9e2c439560e023f5f11e70b4e8
dreinon <danielreinon@outlook.com>
2025-05-16 09:47:44 +0200

    improve: separate into public and secure routers

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Security
+from fastapi import APIRouter, FastAPI, Security
 from fastapi.responses import HTMLResponse
 from lib.barrios import get_barrio_polygon
 from lib.geojson import geojson_to_graph
@@ -9,9 +9,35 @@ from .auth import JWTBearer
 from .lib.models import simulation_M1, simulation_M2, generate_context
 from .models import Input, ModelOutput, SimulateOutput
 
-app = FastAPI(title=__project_name__, dependencies=[Security(JWTBearer())])
+app = FastAPI(title=__project_name__)
 
-@app.post("/simulate/m1/map", response_class=HTMLResponse)
+public_router = APIRouter()
+secured_router = APIRouter(dependencies=[Security(JWTBearer())])
+
+
+# @public_router.get("/simulate/m1/map", response_class=HTMLResponse)
+# def map_m1():
+#     input = Input(
+#         barrio="BENIMACLET",
+#         nodo_entrada=257843726,
+#         almacen="almacen_24814",
+#         num_paquetes=30,
+#     )
+#     G_barrio = geojson_to_graph(f"src/data/supergrafos/{input.barrio}.geojson")
+#     df_comercios = generate_context(G_barrio, input.num_paquetes)
+#     barrio_polygon = get_barrio_polygon(input.barrio)
+
+#     _, mapa_m1 = simulation_M1(
+#         G_barrio,
+#         df_comercios,
+#         input.nodo_entrada,
+#         shp_zone=barrio_polygon,
+#     )
+
+#     return map_to_html(mapa_m1)
+
+
+@secured_router.post("/simulate/m1/map", response_class=HTMLResponse)
 def map_m1(input: Input):
     G_barrio = geojson_to_graph(f"src/data/supergrafos/{input.barrio}.geojson")
     df_comercios = generate_context(G_barrio, input.num_paquetes)
@@ -26,7 +52,8 @@ def map_m1(input: Input):
 
     return map_to_html(mapa_m1)
 
-@app.post("/simulate/m2/map", response_class=HTMLResponse)
+
+@secured_router.post("/simulate/m2/map", response_class=HTMLResponse)
 def map_m2(input: Input):
     G_barrio = geojson_to_graph(f"src/data/supergrafos/{input.barrio}.geojson")
     df_comercios = generate_context(G_barrio, input.num_paquetes)
@@ -43,7 +70,7 @@ def map_m2(input: Input):
     return map_to_html(mapa_m2)
 
 
-@app.post("/simulate/m1")
+@secured_router.post("/simulate/m1")
 def simulate_m1(input: Input) -> ModelOutput:
     G_barrio = geojson_to_graph(f"src/data/supergrafos/{input.barrio}.geojson")
     df_comercios = generate_context(G_barrio, input.num_paquetes)
@@ -59,7 +86,7 @@ def simulate_m1(input: Input) -> ModelOutput:
     return {"results": results_m1, "map_html": map_to_html(mapa_m1)}
 
 
-@app.post("/simulate/m2")
+@secured_router.post("/simulate/m2")
 def simulate_m2(input: Input) -> ModelOutput:
     G_barrio = geojson_to_graph(f"src/data/supergrafos/{input.barrio}.geojson")
     df_comercios = generate_context(G_barrio, input.num_paquetes)
@@ -76,7 +103,7 @@ def simulate_m2(input: Input) -> ModelOutput:
     return {"results": results_m2, "map_html": map_to_html(mapa_m2)}
 
 
-@app.post("/simulate")
+@secured_router.post("/simulate")
 def simulate(input: Input) -> SimulateOutput:
     G_barrio = geojson_to_graph(f"src/data/supergrafos/{input.barrio}.geojson")
     df_comercios = generate_context(G_barrio, input.num_paquetes)
@@ -100,3 +127,7 @@ def simulate(input: Input) -> SimulateOutput:
         "m1": {"results": results_m1, "map_html": map_to_html(mapa_m1)},
         "m2": {"results": results_m2, "map_html": map_to_html(mapa_m2)},
     }
+
+
+app.include_router(public_router, tags=["public"])
+app.include_router(secured_router, tags=["secured"])
